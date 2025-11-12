20c1c596da328b8ae0fe211dfa388992693a302b
davebobculver <david.culver94@gmail.com>
2025-11-12 11:24:32 -0800

    Adding backwash script

---
+++ bw_reg_.py
@@ -0,0 +1,56 @@
+'Train a NN on frames 250 to 350 on backwash and save the net'
+
+'modules set up'
+
+import os
+import multiprocessing
+multiprocessing.set_start_method('spawn', force=True)
+from Modules import pre_pro
+import torch
+from torch.utils.data import TensorDataset, DataLoader
+from Modules.Neural_Net import Net
+from Modules.Losses import crit
+import numpy as np
+from Modules.beams import beam_trim
+import matplotlib.pyplot as plt
+
+
+'250 to 350 shuffled'
+
+data = torch.load('../../data/data.pt', weights_only=True)
+frames = np.arange(250,350, 1)
+device = 'cuda'
+
+beams = list(range(0, 32))
+indices_to_remove = [4, 30]
+beams = [b for i, b in enumerate(beams) if i not in indices_to_remove]
+
+with torch.no_grad():
+    input, output = pre_pro.in_out(data, beams=beams, frames=frames)
+
+
+dataset = pre_pro.FrameDataset(inputs_sorted=input, outputs_sorted= output)
+spt_dataloader = DataLoader(dataset, pin_memory=True,shuffle = True, num_workers=1, persistent_workers= True, batch_size=1)
+
+
+
+'_________________________________'
+
+
+'Adding regularization'
+
+network_info = torch.load('nets/MSE/MSE_250_350.pt', weights_only=True)
+retrain_net = Net(torch.cos, *network_info['model config'])
+retrain_net.load_state_dict(network_info['model_state_dict'])
+retrain_net.to('cuda')
+spt_optimizer = torch.optim.Adam(retrain_net.parameters(), lr = 0 )
+spt_optimizer.load_state_dict(network_info['optimizer_state_dict'])
+for param_group in spt_optimizer.param_groups:
+    param_group['lr'] = 1e-4
+
+if __name__ == '__main__':
+
+    retrainer = pre_pro.temp_nn_wrap('cuda', spt_dataloader, model_config=network_info['model config'],
+                                    optimizer=spt_optimizer, net = retrain_net)
+
+    retrainer.SP_T_train(100, prints=True, saving= True, save_path='nets/SPT/spt_250_350_vwvfvbe.pt', ly =5 )
